618c04f6e5427def56c7e980ea4081e610ae2d0b
Nikhila <nikhilaprab16@gmail.com>
2026-06-09 23:53:19 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+const parent = React.createElement(
+    "div", 
+    {id:"parent"}, 
+    [React.createElement("h1",{},"I am H1 tag"),
+    React.createElement("h2",{},"I am H2 tag")]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
